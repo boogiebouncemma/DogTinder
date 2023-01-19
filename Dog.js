0bafdc4a58df359307688efff1ad05e43bc7a8a7
Boogie <85059214+boogiebouncemma@users.noreply.github.com>
2023-01-19 14:54:26 +0100

    Update Dog.js

--- Dog.js
+++ Dog.js
@@ -17,7 +17,7 @@ class Dog {
     getDogHtml() {
         return `
             <div id="dog--avatar-container">
-                <img src="../${this.avatar}" class="dog--avatar"/>
+                <img src="./${this.avatar}" class="dog--avatar"/>
             </div>
 
             <div class="dog--text-container">
